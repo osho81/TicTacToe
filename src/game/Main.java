d48154fdbe4847f7a313e6a382c6436fd41f7e6f
Mohamad Ojail <mohamad.ojail@yaelev.se>
2021-10-28 22:44:39 +0200

    Mohamad pulled the repository... Signed-off-by: Mohamad Ojail <mohamad.ojail@yaelev.se>

--- src/game/Main.java
+++ src/game/Main.java
@@ -7,6 +7,17 @@ public class Main {
 		System.out.println("Katarina");
 		System.out.println("Osama");
 		System.out.println("Tack Osama och Mohamad");
+		
+		System.out.println();
+		for( int i = 0, x = 1; i <= 3; i++) {
+			
+			for( int j = 0; j <= 3; j++, x++) {
+				System.out.print(x + "\t");
+			}
+			System.out.println();
+		}
+		System.out.println("Mohamad skrev ut for loopen!");
+		
 	}
 
 }
